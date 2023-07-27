ea6aa70f7e7414171d2569b9f41e64a1155e1dc8
Nikita <varashilov125@gmail.com>
2023-07-27 17:09:49 +0300

    Programma

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] Array;
+Array = GetArray();
+
+Console.WriteLine($"Элементы массива введенные пользователем = [{String.Join(", ", Array)}]");
+
+
+string[] GetArray()
+{
+    Console.Write("Введите элементы массива через пробел: ");
+    string Array = Console.ReadLine()!;
+    string[] stringsArray = Array.Split(' ');
+
+    return stringsArray;
+}
+
+void PrintArray(string[] array)
+{
+    int size = array.Length;
+    int length = 0;
+    int newArraySize = 0;
+    for (int i = 0; i < size; i++)
+    {
+        length = array[i].Length;
+        if (length <= 3)
+        {
+            newArraySize++;
+        }
+
+    }
+    if (newArraySize == 0)
+    {
+        Console.Write("[]");
+        return;
+    }
+    string[] newArray = new string[newArraySize];
+    int j = 0;
+    Console.Write("[");
+    for (int i = 0; i < size; i++)
+    {
+        length = array[i].Length;
+
+        if (length <= 3)
+        {
+            newArray[j] = array[i];
+            Console.Write(newArray[j] + " ");
+            j++;
+        }
+
+    }
+    Console.Write("]");
+}
+
+PrintArray(Array);
